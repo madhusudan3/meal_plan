fcbc9f7fc0a1011b2d459718436d018436b025fa
Madhu <madhusudan.koukutla@gmail.com>
2018-08-29 12:45:09 -0500

    Modeling a Recipe and Testing

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -29,5 +28,9 @@ module MealPlan
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+        g.test_framework :minitest, spec: true
+    end
   end
 end
